3879e7cd8b209d8c61928978f3521994de88f0c5
parkmingue00 <parkm2ngyu00@naver.com>
2022-03-04 18:51:34 +0900

    Create 9_bs4_coupang.py

---
+++ website_basic/9_bs4_coupang.py
@@ -0,0 +1,43 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = "https://www.coupang.com/np/search?q=%EB%85%B8%ED%8A%B8%EB%B6%81&channel=user&component=&eventCategory=SRP&trcid=&traid=&sorter=scoreDesc&minPrice=&maxPrice=&priceRange=&filterType=&listSize=36&filter=&isPriceRange=false&brand=&offerCondition=&rating=0&page=1&rocketAll=false&searchIndexingToken=1=5&backgroundColor="
+hearders = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"}
+res = requests.get(url, headers=hearders)
+res.raise_for_status
+soup = BeautifulSoup(res.text, "lxml")
+
+items = soup.find_all("li", attrs={"class": re.compile("^search-product")})
+# print(items[0].find("div", attrs={"class": "name"}).get_text())
+
+for item in items:
+
+    # 광고 제품은 제외
+    ad_badge = item.find("span", attrs={"class":"ad-badge-text"})
+    if ad_badge :
+        print("<광고 상품 제외합니다>")
+        continue
+
+    name = item.find("div", attrs={"class": "name"}).get_text()
+
+    # 애플 제품 제외
+    if "Apple" in name :
+        print("애플 상품 제외합니다.")
+        continue
+
+    price = item.find("strong", attrs={"class": "price-value"}).get_text()
+
+    # 리뷰 100개 이상, 평점 4.5 이상 되는 것만 조회
+    rate = item.find("em", attrs={"class": "rating"})
+    rate_count = item.find("span", attrs={"class": "rating-total-count"})
+    if rate and rate_count :
+        rate = rate.get_text()
+        rate_count = rate_count.get_text()
+        rate_count = rate_count[1 : -1]
+    else :
+        print("<평점 없는 상품 제외합니다.>")
+        continue
+    
+    if float(rate) >= 4.5 and int(rate_count) >= 100: 
+        print(name, price, rate, rate_count)
