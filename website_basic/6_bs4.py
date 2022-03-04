85aadf30d6c1e29dd72426b62b0caaa0da386657
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2022-03-04 11:36:51 +0900

    Update 6_bs4.py

--- website_basic/6_bs4.py
+++ website_basic/6_bs4.py
@@ -15,9 +15,22 @@ soup = BeautifulSoup(res.text, "lxml")
 
 # class="" 인 a element 를 찾아줘
 # print(soup.find("a", attrs={"class": "Nbtn_upload"}))
-# # class="" 인 element 를 찾아줘
+# class="" 인 element 를 찾아줘
 # print(soup.find(attrs={"class": "Nbtn_upload"}))
 
 # print(soup.find("li", attrs={"class": "rank01"}))
-rank1 = soup.find("li", attrs={"class": "rank01"})
-print(rank1.a)
+# rank1 = soup.find("li", attrs={"class": "rank01"})
+# print(rank1.a.get_text())
+# rank2 = rank1.next_sibling.next_sibling
+# rank3 = rank2.next_sibling.next_sibling
+# print(rank3.a.get_text())
+# rank2 = rank3.previous_sibling.previous_sibling
+# print(rank2)
+# print(rank1.parent)
+# rank2 = rank1.find_next_sibling("li")
+# print(rank2.a.get_text())
+
+# print(rank1.find_next_siblings("li"))
+
+webtoon = soup.find("a", text="독립일기-11화 밥공기 딜레마")
+print(webtoon)
