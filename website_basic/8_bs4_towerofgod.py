fcf5731a373672491d53dff5af90e218bc76edc9
parkmingue00 <parkm2ngyu00@naver.com>
2022-03-04 18:51:31 +0900

    Update 8_bs4_towerofgod.py

--- website_basic/8_bs4_towerofgod.py
+++ website_basic/8_bs4_towerofgod.py
@@ -7,13 +7,21 @@ res = requests.get(url)
 res.raise_for_status
 
 soup = BeautifulSoup(res.text, "lxml")
-cartoons = soup.find_all("td", attrs={"class" : "title"})
-# title = cartoons[0].a.get_text()
-# link = cartoons[0].a["href"]
-# print(title)
-# print("https://comic.naver.com" + link)
+cartoons = soup.find_all("td", attrs={"class": "title"})
 
+# 만화 제목 + 링크 가져오기
 for cartoon in cartoons:
     title = cartoon.a.get_text()
     link = "https://comic.naver.com" + cartoon.a["href"]
     print(title, link)
+
+# 평점 구하기
+cartoons = soup.find_all("div", attrs={"class": "rating_type"})
+totalrate = 0
+i = 0
+for cartoon in cartoons:
+    rate = float(cartoon.strong.get_text())
+    i += 1
+    totalrate += rate
+
+print(totalrate / i)
