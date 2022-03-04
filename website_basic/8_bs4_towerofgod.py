0778bc01feaba631270986c9d0db9e33997813a2
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2022-03-04 11:37:12 +0900

    Create 8_bs4_towerofgod.py

---
+++ website_basic/8_bs4_towerofgod.py
@@ -0,0 +1,19 @@
+from wsgiref.util import request_uri
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://comic.naver.com/webtoon/list?titleId=183559&weekday=mon"
+res = requests.get(url)
+res.raise_for_status
+
+soup = BeautifulSoup(res.text, "lxml")
+cartoons = soup.find_all("td", attrs={"class" : "title"})
+# title = cartoons[0].a.get_text()
+# link = cartoons[0].a["href"]
+# print(title)
+# print("https://comic.naver.com" + link)
+
+for cartoon in cartoons:
+    title = cartoon.a.get_text()
+    link = "https://comic.naver.com" + cartoon.a["href"]
+    print(title, link)
