08c72fb852aae69fbd5b666241fb150e7ac879ca
park min kyu <88785472+parkmingue00@users.noreply.github.com>
2022-03-06 19:37:27 +0900

    delete old directory

--- website_basic/5_user_agent.py
+++
@@ -1,9 +0,0 @@
-import requests
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36"}
-url = "http://nadocoding.tistory.com"
-res = requests.get(url, headers=headers)
-res.raise_for_status()
-
-with open("nadocoding.html", "w", encoding="utf8") as f:
-    f.write(res.text)
